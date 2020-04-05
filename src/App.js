73adbf7a59db2df3f01147695ec6fcdd746cc5cf
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-04 20:43:43 -0400

    pasted routing excersice as template

--- src/App.js
+++ src/App.js
@@ -1,1 +1,41 @@
-
+import {Route,Switch, Link , BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Category from './components/Category'
+import Products from './components/Products.js';
+import Login from './components/Login.js';
+ 
+function App() {
+ return (
+   <Router>
+     <div className="App">
+       <nav>
+         <ul>
+           <li>
+             <Link to="/Home">Home</Link>
+           </li>
+           <li>
+             <Link to="/Category">Category</Link>
+           </li>
+           <li>
+             <Link to="/Product">Products</Link>
+           </li>
+           <li>
+             <Link to="/Login">Login</Link>
+           </li>
+         </ul>
+       </nav>
+ 
+       <Switch>
+         <Route exact path="/Home" component={Home} />
+         <Route exact path="/Category" component={Category} />
+         <Route exact path="/Product" component={Products} />
+         <Route exact path="/Login" component={Login} />
+       </Switch>
+     </div>
+   </Router>
+ 
+ );
+}
+ 
+export default App;
---
+++ src/Brooklyn.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ 
+function Home() {
+   return (
+       <React.Fragment >
+       <h1>Welcome to my Home</h1>
+ 
+      
+       </React.Fragment>
+ 
+   )
+ 
+}
+ 
+export default Home
