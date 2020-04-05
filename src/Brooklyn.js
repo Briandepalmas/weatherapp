d28d622d81be9f69b1d5bd2c59327c5ca93ceda5
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-05 00:05:40 -0400

    fixed api error by adding https:// in front of url

--- src/Brooklyn.js
+++ src/Brooklyn.js
@@ -1,11 +1,12 @@
 import React from 'react'
  
 function Brooklyn() {
+   // "id":5110302
    return (
        <React.Fragment >
        <h1>Welcome to Brooklyn</h1>
  
-       "id":5110302
+       
        </React.Fragment>
  
    )
--- src/SantoDomingo.js
+++ src/SantoDomingo.js
@@ -4,36 +4,37 @@ import axios from 'axios'
 export class SantoDomingo extends Component {
   constructor(props) {
     super(props);
-  
+    //"id":3492908
     this.state = {
       data: []
     }
   }
 componentDidMount(){
-    let fiveDays =  "api.openweathermap.org/data/2.5/forecast?id=524901&appid=e9984ab7f3bed98ab978f1bfc5c63170"
-const forecast=axios.get(fiveDays)
-   forecast.then(res => {
-     const weather= res.data;
+    //let fiveDays ="api.openweathermap.org/data/2.5/forecast?id=3492908&appid=e9984ab7f3bed98ab978f1bfc5c63170"
+const forecast=axios.get("https://api.openweathermap.org/data/2.5/forecast?id=5110302&appid=e9984ab7f3bed98ab978f1bfc5c63170")
+   forecast.then(response => {
+     let weather= response.data.city.name;
      console.log("recipes ---->", weather)
     
      this.setState({data: weather})
  
    })
    .catch(error => {
-     console.log('there is an error', error)
+     console.log('there is an error', error.response)
    })
  
  }
 
     render() {
         return (
+          <React.Fragment >
             <div>
-              "id":3492908
                 <h1>Welcome to Santo Domingo</h1>
                 <ul>
                   <li>{this.state.data} </li>
                 </ul>
             </div>
+          </React.Fragment>
         )
     }
 }
