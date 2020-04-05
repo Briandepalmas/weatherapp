ee5a55a13cc20feba36fbd899750b4cf1758d190
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-05 01:34:47 -0400

    implemented high and low temp for brooklyn

--- src/Brooklyn.js
+++ src/Brooklyn.js
@@ -14,10 +14,11 @@ componentDidMount(){
    
 const forecast=axios.get("https://api.openweathermap.org/data/2.5/forecast?id=5110302&appid=e9984ab7f3bed98ab978f1bfc5c63170")
    forecast.then(response => {
-     let weather= response.data.list[1].main.temp;
-     console.log("recipes ---->", weather)
+    let high= response.data.list[1].main.temp_max;
+    let low= response.data.list[1].main.temp_min;
+     console.log("recipes ---->", low)
     
-     this.setState({data: weather})
+     this.setState({data: [high,low]})
  
    })
    .catch(error => {
@@ -36,7 +37,8 @@ const forecast=axios.get("https://api.openweathermap.org/data/2.5/forecast?id=51
           <React.Fragment >
             <div>
                 <h1>Welcome to Brooklyn
-                  the temperature is {this.state.data}F 
+                Highest temperature is {this.state.data[0]}F
+                  Lowest temperature is {this.state.data[1]}F 
                 </h1>
                 
             </div>
